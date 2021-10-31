65087c1991a109dbb1b3d27cd9612ffe7aa94f04
Glitch (shrub-tarry-gladiolus) <none>
2021-10-31 18:43:00 +0000

    🐔🙈 Checkpoint ./package.json:37866854/22 ./server.js:37866854/508

--- server.js
+++ server.js
@@ -1,13 +1,4 @@
-const dbd = require("dbd.js")
- 
 const bot = new dbd.Bot({
   token: "OTAzNTk2OTkxNTMwMDIwOTE0.YXvSRg.HmcDehUnyW4wcT0L_vW0gT6HqaU", 
   prefix: "="
-})
- 
-bot.onMessage()
- 
-bot.command({
-  name: "ping",
-  code: `Pong! \`$ping\` ms`
 })
